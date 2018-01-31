29b51e497c5da17656d1561b319253439ff10d40
johofukyu <gsi-ina@ml.mlit.go.jp>
2018-01-31 15:18:50 +0900

    Update gsiglobe.js、layers*.txt、Add layers*.txt

--- globe/resource/gsiglobe.js
+++ globe/resource/gsiglobe.js
@@ -61,17 +61,10 @@ CONFIG.layersTab = [
 		'caption' : 'トピック',
 		'layers' : [
 			'../layers_txt/layers1.txt',
-			'../layers_txt/layers_1003.txt',
 			'../layers_txt/layers_topic.txt',
 			'../layers_txt/layers_skhb.txt',
-			'../layers_txt/layers_typhoon.txt',
+			'../layers_txt/layers_topic_kusatsushirane.txt',
 			'../layers_txt/layers_topic_nishi.txt'
-		]
-	},
-	{
-		'caption' : '台風・豪雨等',
-		'layers' : [
-			'../layers_txt/layers_typhoon.txt'
 		]
 	},
 	{
